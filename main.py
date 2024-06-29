6acbcb0e1e0672f1cf863dd8abcb7a10faa934d3
Advick Bhalla-allrounder <130120619+deadpanpulley@users.noreply.github.com>
2024-06-29 09:31:51 +0530

    Add files via upload
    🚀 Exciting News! I've Built a Chat Application Using React and Chat Engine API! 🚀
    
    I'm thrilled to share that I've recently developed a fully functional chat application leveraging the power of React and the Chat Engine API. This project has been an incredible learning experience and a significant step forward in my journey as a developer.
    
    🛠️ Key Features:
    
    Real-time Messaging: Seamless, instant communication.
    User Authentication: Secure login and user management.
    Customizable UI: A sleek and responsive design tailored for the best user experience.
    Scalability: Built to handle a growing user base with ease.
    💡 Why It Matters:
    Creating this application has not only deepened my understanding of front-end development and API integration but also showcased the potential of modern web technologies in building dynamic and interactive user experiences.
    
    
    
    I'm eager to hear your thoughts and feedback! If you're interested in learning more about the development process or have any questions, feel free to reach out.
    
    #React #ChatEngineAPI #WebDevelopment #FrontEndDevelopment #TechInnovatio

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+import requests
+PRIVATE_KEY = "85b3344e-605e-4db4-98be-6e53f766d16a"
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
+
+ 
+    
